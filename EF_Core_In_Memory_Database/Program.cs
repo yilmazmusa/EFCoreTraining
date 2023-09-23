e8c6c6f104bbcb778eac8185a3b890dae20b16aa
Musa YILMAZ <musa.yilmaz@param.com.tr>
2023-09-23 13:43:05 +0300

    In-Memory Database çalışması  eklendi.

--- EF_Core_In_Memory_Database/Program.cs
+++ EF_Core_In_Memory_Database/Program.cs
@@ -10,23 +10,28 @@ ApplicationDbContext context = new();
 //Dolayısıyla özellikle gerçek uygulamalarda in-memory database'i kullanıyorsanız bunun kalıcı değil geçici yani silinebilir bir özellik olduğunu UNUTMAYIN!
 
 #region EF Core'da In-Memory Database İle Çalışmanın Gereği Nedir?
-//Ben deniz(Gençay) genellikle bu özelliği yeni çıkan EF Core özelliklerini test edebilmek için kullanıyorum. 
+//Ben deniz(Musa) genellikle bu özelliği yeni çıkan EF Core özelliklerini test edebilmek için kullanıyorum. 
 //EF Core, fiziksel veritabanlarından ziyade in-memory'de Database oluşturup üzerinde birçok işlemi yapmamızı sağlayabilmektedir. İşte bu özellik ile gerçek uygulamaların dışında test gibi operasyonları hızlıca yürütebileceğimiz imkanlar elde edebilmekteyiz.
 #endregion
+
 #region Avantajları Nelerdir?
-//Test ve pre-prod uygulamalarda gerçek/fiziksel veritabanları oluşturmak ve yapılandıormak yerine tüm veritanını bellekte modelleyebilir ve gerekli işlemleri sanki gerçek bir veritabanında çalışıyor gibi orada gerçekleştirebiliriz.
+//Test ve pre-prod uygulamalarda gerçek/fiziksel veritabanları oluşturmak ve yapılandırmak yerine tüm veritanını bellekte modelleyebilir ve gerekli işlemleri sanki gerçek bir veritabanında çalışıyor gibi orada gerçekleştirebiliriz.
 //Bellekte çalışmak geçici bir deneyim olacağı için veritabanı serverlarında test amaçlı üretilmiş olan veritabanlarının lüzumsuz yer işgal etmesini engellemiş olacaktır.
 //Bellekte veritabanını modellemek kodun hızlı bir şekilde test edilmesini sağlayacaktır
 #endregion
+
 #region Dezavantajları Nelerdir?
 //In-Memory'de yapılacak olan veritabanı işlevlerinde ilişkisel modellemeler YAPILAMAMAKTADIR! Bu durumdan dolayı veri tutarlılığı sekteye uğrayabilir ve istatiksel açıdan yanlış sonuçlar elde edilebilir.
 #endregion
+
 #region Örnek Çalışma
 //Microsoft.EntityFrameworkCore.InMemory kütüphanesi uygulamaya yüklenmelidir.
-await context.Persons.AddAsync(new() { Name = "Gençay", Surname = "Yıldız" });
+await context.Persons.AddAsync(new() { Name = "Musa", Surname = "Yılmaz" });
 await context.SaveChangesAsync();
 
 var persons = await context.Persons.ToListAsync();
+ var eklenen_person =  await context.Persons.Where(p => p.Id == 1).ToListAsync();
+
 Console.WriteLine();
 #endregion
 
@@ -42,6 +47,6 @@ class ApplicationDbContext : DbContext
     public DbSet<Person> Persons { get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseInMemoryDatabase("exampleDatabase");
+        optionsBuilder.UseInMemoryDatabase("ExampleDatabase"); //In-Memory de ExampleDatabase adında bir VT oluşturuduk.In-Memory de oludğu için migration işlemleri yapmıyoruz.
     }
 }
